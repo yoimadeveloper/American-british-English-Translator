4aba5f6bd5ab89526655fb1f689c982c4d6457c5
mohit <mohitpanwar8484@gmail.com>
2021-06-17 17:59:24 +0530

    hey check this out

---
+++ routes/api.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const Translator = require('../components/translator.js');
+
+module.exports = function(app) {
+
+  const translator = new Translator();
+  const valid_locales = ['american-to-british', 'british-to-american'];
+
+  app.route('/api/translate')
+    .post((req, res) => {
+      if (req.body.hasOwnProperty('text') &&
+        req.body.hasOwnProperty('locale')) {
+        if (req.body.text === '') {
+          res.json({error: 'No text to translate'});
+        } else if (!valid_locales.includes(req.body.locale)) {
+          res.json({error: 'Invalid value for locale field'});
+        } else {
+          translator.setText(req.body.text);
+          translator.setLocale(req.body.locale);
+          translator.translate();
+          res.json({
+            'text': translator.text,
+            'translation': translator.translation,
+          });
+        }
+      } else {
+        res.json({'error': 'Required field(s) missing'});
+      }
+    });
+};
---
+++ tests/1_unit-tests.js
@@ -0,0 +1,19 @@
+const chai = require('chai');
+const assert = chai.assert;
+
+const Translator = require('../components/translator.js');
+const testCases = require('../components/unitTestDefinitions.js');
+
+suite('Unit Tests ->', () => {
+  const translator = new Translator();
+
+  testCases.forEach((unitTest) => {
+    test(`${unitTest.text} -> ${unitTest.translation}`, () => {
+      translator.setText(unitTest.text);
+      translator.setLocale(unitTest.locale);
+      translator.setHighlight(unitTest.highlight);
+      translator.translate();
+      assert.equal(translator.translation, unitTest.translation);
+    });
+  });
+});
